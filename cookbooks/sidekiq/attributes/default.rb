d36a8ee32b80ac649d42cbaf54fbf3249efd82a9
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-12-01 16:17:52 -0800

    [DOKI] enables sidekiq recipe

--- cookbooks/sidekiq/attributes/default.rb
+++ cookbooks/sidekiq/attributes/default.rb
@@ -9,19 +9,20 @@ default[:sidekiq] = {
   # only be installed on to a utility instance that matches
   # the name
   :utility_name => 'sidekiq',
-  
+
   # Number of workers (not threads)
   :workers => 1,
-  
+
   # Concurrency
   :concurrency => 25,
-  
+
   # Queues
   :queues => {
     # :queue_name => priority
-    :default => 1
+    :default => 1,
+    :mailers => 1
   },
-  
+
   # Verbose
   :verbose => false
 }
